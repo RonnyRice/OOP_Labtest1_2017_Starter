28251b3ca1b91c92e05cf8aedebdb583a2b66e75
RonnyRice <82329993+RonnyRice@users.noreply.github.com>
2022-04-26 23:40:46 +0100

    asd

---
+++ java/src/ie/tudublin/Main.java
@@ -0,0 +1,17 @@
+package ie.tudublin;
+
+public class Main
+{
+
+    public void gant()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new cafe());
+    }
+    
+    public static void main(String[] arg)
+    {
+        Main main = new Main();
+		main.gant();        
+    }
+}
